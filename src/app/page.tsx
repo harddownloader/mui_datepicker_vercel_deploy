a9eb7ee814ed7c8c2d468b3b5bde506d55caeb37
harddownloader <serafimdev@gmail.com>
2023-06-07 18:57:10 +0300

    rm componentsProps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,52 @@
+'use client'
 import Image from 'next/image'
 import styles from './page.module.css'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+import { CalendarToday } from "@mui/icons-material"
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns"
+import ruLocale from "date-fns/locale/ru"
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
+import SearchIcon from '@mui/icons-material/Search'
+import NorthEastOutlinedIcon from '@mui/icons-material/NorthEastOutlined'
+import SouthEastOutlinedIcon from '@mui/icons-material/SouthEastOutlined'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <>
+    <LocalizationProvider
+              dateAdapter={AdapterDateFns}
+              // adapterLocale={ruLocale}
+            >
+              <DatePicker
+                label="Дата поездки"
+                // value={new Date()}
+                // onChange={() => {}}
+                // minDate={new Date()}
+                // className={
+                //   `${styles.date_picker__container} ${classNames}`
+                // }
+                // ref={refDatePicker}
+                // onClose={() => setIsOpen(false)}
+                // open={isOpen}
+                // onOpen={() => setIsOpen(true)}
+                // componentsProps={{
+                //   textField: {
+                //     // WHY it isn't works?
+                //     InputProps: {
+                //       // ...InputProps,
+                //       startAdornment: (
+                //         <InputAdornment position="start">
+                //           <CalendarToday />
+                //         </InputAdornment>
+                //       ),
+                //       endAdornment: null
+                //     },
+                //     // className: `${styles.date_picker__container} ${styles.date_picker__input} ${searchTicketsStyles.searchField} ${isLastElementInRow ? styles.last_el : ''}`,
+                //     // onClick: (e) => setIsOpen(true),
+                //   }
+                // }}
+              />
+            </LocalizationProvider>
+    </>
   )
 }
