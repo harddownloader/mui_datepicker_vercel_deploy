0d6da16ef668496b2bcd8fae2d7802792ecbcf61
harddownloader <serafimdev@gmail.com>
2023-06-07 19:18:50 +0300

    lint page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,39 +14,39 @@ export default function Home() {
   return (
     <>
     <LocalizationProvider
-              dateAdapter={AdapterDateFns}
-              // adapterLocale={ruLocale}
-            >
-              <DatePicker
-                label="Дата поездки"
-                // value={new Date()}
-                // onChange={() => {}}
-                // minDate={new Date()}
-                // className={
-                //   `${styles.date_picker__container} ${classNames}`
-                // }
-                // ref={refDatePicker}
-                // onClose={() => setIsOpen(false)}
-                // open={isOpen}
-                // onOpen={() => setIsOpen(true)}
-                // componentsProps={{
-                //   textField: {
-                //     // WHY it isn't works?
-                //     InputProps: {
-                //       // ...InputProps,
-                //       startAdornment: (
-                //         <InputAdornment position="start">
-                //           <CalendarToday />
-                //         </InputAdornment>
-                //       ),
-                //       endAdornment: null
-                //     },
-                //     // className: `${styles.date_picker__container} ${styles.date_picker__input} ${searchTicketsStyles.searchField} ${isLastElementInRow ? styles.last_el : ''}`,
-                //     // onClick: (e) => setIsOpen(true),
-                //   }
-                // }}
-              />
-            </LocalizationProvider>
+      dateAdapter={AdapterDateFns}
+      // adapterLocale={ruLocale}
+    >
+      <DatePicker
+        label="Дата поездки"
+        // value={new Date()}
+        // onChange={() => {}}
+        // minDate={new Date()}
+        // className={
+        //   `${styles.date_picker__container} ${classNames}`
+        // }
+        // ref={refDatePicker}
+        // onClose={() => setIsOpen(false)}
+        // open={isOpen}
+        // onOpen={() => setIsOpen(true)}
+        // componentsProps={{
+        //   textField: {
+        //     // WHY it isn't works?
+        //     InputProps: {
+        //       // ...InputProps,
+        //       startAdornment: (
+        //         <InputAdornment position="start">
+        //           <CalendarToday />
+        //         </InputAdornment>
+        //       ),
+        //       endAdornment: null
+        //     },
+        //     // className: `${styles.date_picker__container} ${styles.date_picker__input} ${searchTicketsStyles.searchField} ${isLastElementInRow ? styles.last_el : ''}`,
+        //     // onClick: (e) => setIsOpen(true),
+        //   }
+        // }}
+      />
+      </LocalizationProvider>
     </>
   )
 }
